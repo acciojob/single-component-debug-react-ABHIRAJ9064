854cb3700a14a5bc2c71ca421bccd90b892b1e3c
ABHIRAJ9064 <kabhiraj903@gmail.com>
2024-07-13 19:42:47 +0530

    finished

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,10 @@ import '../styles/App.css';
 const App = () => {
   return (
     <div id="main">
-      <h1 data-ns-test="project-name">My Projeect</h1>
-      <p data-ns-test="project-description">This a description of my project.</p>
+      <p>I am learning React. My life is getting better.</p>
     </div>
   )
 }
 
 
 export default App;
-
